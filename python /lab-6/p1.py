399f9f4371a55d04bf256464ccea1d4ff786e869
Gaurav <102450738+gau-rhv@users.noreply.github.com>
2024-09-03 06:37:38 +0000

    changes

--- python /lab-6/p1.py
+++ python /lab-6/p1.py
@@ -1,17 +1,13 @@
-def count_digits_letters(s):
-    d = 0
-    l = 0
-
-    for c in s:
-        if c.isdigit():
-            d += 1
-        elif c.isalpha():
-            l += 1
-
-    return d, l
-
 s = input("Enter a sentence: ")
-d, l = count_digits_letters(s)
+
+d = 0
+l = 0
+
+for c in s:
+    if c.isdigit():
+        d += 1
+    elif c.isalpha():
+        l += 1
 
 print("Number of digits:", d)
 print("Number of letters:", l)
